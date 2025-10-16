550c0278162f45ff1899b4323b988c5085e41491
pavel <pavelpalonko@gmail.com>
2025-10-17 00:22:15 +0300

    added carousel component

--- app/src/modules/datepicker/components/datepicker-day-swiper.tsx
+++ app/src/modules/datepicker/components/datepicker-day-swiper.tsx
@@ -2,37 +2,63 @@ import { format, getDate } from "date-fns";
 
 // context
 import { useDatepickerContext } from "../composer/datepicker-composer";
+import { CarouselComponent } from "@/modules/carousel/carousel-component";
 
 export function DaySwiper() {
   const { state, actions } = useDatepickerContext();
 
   return (
-    <div style={{ display: "flex", gap: "20px", paddingTop: "30px" }}>
-      {state.slots.map((day, index) => (
+    <div>
+      <CarouselComponent>
         <div
-          key={day.date}
-          style={{ position: "relative" }}
-          onClick={() => actions.setSelectedDay(day)}
+          style={{
+            display: "flex",
+            width: "300px",
+            gap: "15px",
+            padding: "20px",
+          }}
         >
-          {/* only for first elemet of mounth */}
-          {(index === 0 || getDate(day.date) === 1) && (
-            <div
-              style={{
-                top: "-12px",
-                fontSize: "12px",
-                position: "absolute",
-                backgroundColor: "gray",
-              }}
-            >
-              {format(day.date, "MMM")}
-            </div>
-          )}
+          <CarouselComponent.PrevButton />
 
-          <div>{format(day.date, "EE")}</div>
+          <CarouselComponent.Viewport>
+            {state.slots.map((day, index) => (
+              <div
+                key={day.date}
+                style={{
+                  backgroundColor: "gold",
+                  width: "40px",
+                  height: "60px",
+                  display: "flex",
+                  alignItems: "end",
+                }}
+                onClick={() => actions.setSelectedDay(day)}
+              >
+                <div style={{ position: "relative" }}>
+                  {/* only for first elemet of mounth */}
+                  {(index === 0 || getDate(day.date) === 1) && (
+                    <div
+                      style={{
+                        top: "-12px",
+                        fontSize: "12px",
+                        position: "absolute",
+                        backgroundColor: "gray",
+                      }}
+                    >
+                      {format(day.date, "MMM")}
+                    </div>
+                  )}
 
-          <div>{format(day.date, "dd")}</div>
+                  <div>{format(day.date, "EE")}</div>
+
+                  <div>{format(day.date, "dd")}</div>
+                </div>
+              </div>
+            ))}
+          </CarouselComponent.Viewport>
+
+          <CarouselComponent.NextButton />
         </div>
-      ))}
+      </CarouselComponent>
     </div>
   );
 }
--- app/src/modules/datepicker/components/datepicker-time-swiper.tsx
+++ app/src/modules/datepicker/components/datepicker-time-swiper.tsx
@@ -2,6 +2,7 @@ import { format } from "date-fns";
 
 // context
 import { useDatepickerContext } from "../composer/datepicker-composer";
+import { CarouselComponent } from "@/modules/carousel/carousel-component";
 
 export function TimeSwiper() {
   const { state, actions } = useDatepickerContext();
@@ -12,11 +13,25 @@ export function TimeSwiper() {
 
   return (
     <div>
-      {state.selectedDay.times.map((slot) => (
-        <button key={slot.time} onClick={() => actions.setSelectedTime(slot)}>
-          {format(slot.time, "h:mm a")}
-        </button>
-      ))}
+      <CarouselComponent>
+        <div style={{ display: "flex", width: "300px", gap: "15px" }}>
+          <CarouselComponent.PrevButton />
+
+          <CarouselComponent.Viewport>
+            {state.selectedDay.times.map((slot) => (
+              <button
+                key={slot.time}
+                style={{ width: "64px" }}
+                onClick={() => actions.setSelectedTime(slot)}
+              >
+                {format(slot.time, "h:mm a")}
+              </button>
+            ))}
+          </CarouselComponent.Viewport>
+
+          <CarouselComponent.NextButton />
+        </div>
+      </CarouselComponent>
     </div>
   );
 }
