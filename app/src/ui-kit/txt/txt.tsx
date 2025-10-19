dd0e08defbbdd8b676f74197a44fa6ded8371715
pavel <pavelpalonko@gmail.com>
2025-10-20 01:43:01 +0300

    remove unused log

--- app/src/ui-kit/txt/txt.tsx
+++ app/src/ui-kit/txt/txt.tsx
@@ -6,13 +6,13 @@ import styles from "./txt.module.css";
 type Props = {
   tagName?: keyof Pick<JSX.IntrinsicElements, "p" | "span" | "div">;
   color?: "default" | "description" | "light" | "inherit";
-  align?: "center" | "left" | "right";
+
   size?: "small" | "medium" | "large";
   weight?: "light" | "semiBold" | "bold";
   keepWord?: boolean;
   textEllipsis?: boolean;
   lineClamp?: number;
-  transform?: "uppercase" | "lowercase" | "capitalize";
+
   title?: string;
   children: React.ReactNode;
 };
@@ -26,11 +26,9 @@ export function Txt({
   textEllipsis,
   lineClamp = 1,
   weight = "light",
-  align = "left",
   title,
-  transform,
 }: Props) {
-  const classes = classNames(styles[align], {
+  const classes = classNames({
     // text type
     [styles["content-text-small"]]: size === "small",
     [styles["content-text-medium"]]: size === "medium",
@@ -45,11 +43,6 @@ export function Txt({
     [styles["color-light"]]: color === "light",
     [styles["color-default"]]: color === "default",
     [styles["color-description"]]: color === "description",
-
-    // transform
-    [styles.uppercase]: transform === "uppercase",
-    [styles.lowercase]: transform === "lowercase",
-    [styles.capitalize]: transform === "capitalize",
 
     // word-break
     [styles["keep-word"]]: keepWord,
