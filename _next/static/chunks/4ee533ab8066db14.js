ae6f4e35114b2b69aeb41fe643aee3bec8da2d6b
pavel <pavelpalonko@gmail.com>
2025-10-20 17:08:50 +0300

    Updates

--- _next/static/chunks/4ee533ab8066db14.js
+++ _next/static/chunks/4ee533ab8066db14.js
@@ -1,5 +1,5 @@
 __turbopack_load_page_chunks__("/_app", [
   "static/chunks/e60ef129113f6e24.js",
   "static/chunks/e30b228f990159c5.js",
-  "static/chunks/turbopack-9d09104d545778af.js"
+  "static/chunks/turbopack-209307299286a42a.js"
 ])
--- _next/static/chunks/589a53c9c8512d02.js
+++ _next/static/chunks/589a53c9c8512d02.js
@@ -1,5 +1,5 @@
 __turbopack_load_page_chunks__("/_error", [
   "static/chunks/17722e3ac4e00587.js",
   "static/chunks/e30b228f990159c5.js",
-  "static/chunks/turbopack-da420b59affcbf33.js"
+  "static/chunks/turbopack-30257b54cb82cf83.js"
 ])
